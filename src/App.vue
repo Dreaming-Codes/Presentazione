84845a643882216641498da5eb36d9690fe9ff63
Lorenzo Rizzotti <lore.rizzotti@gmail.com>
2021-02-09 23:47:06 +0100

    COSE

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,18 @@
 
 <script>
 import Init from './components/Init.vue'
+import Vue from 'vue'
+import VueNativeSock from 'vue-native-websocket'
 
 
 export default {
   name: 'App',
+  created() {
+    Vue.use(VueNativeSock, 'ws://localhost:8083/pagesocket', {
+      reconnection: true, // (Boolean) whether to reconnect automatically (false)
+      reconnectionDelay: 5000,
+    })
+  },
   components: {
     Init
   }
--- src/components/Init.vue
+++ src/components/Init.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="all">
+    <div id="qrcode" ref="qrcode"></div>
     <div @click="translatetopleft('logo')">
       <img id="logo" src="@/assets/logo-Castelnuovo.png"/>
     </div>
     <div id="text">
-      <vue-typed-js @onComplete="translate" :strings="['DreamingCodes', 'Lorenzo Rizzotti', 'Castelnuovo']">
+      <vue-typed-js :strings="['DreamingCodes', 'Lorenzo Rizzotti', 'Castelnuovo']" @onComplete="translate">
         <h1 class="typing"></h1>
       </vue-typed-js>
     </div>
@@ -14,16 +15,40 @@
 <script>
 import Vue from 'vue'
 import VueTypedJs from 'vue-typed-js'
+import * as QRCode from 'easyqrcodejs'
 
 Vue.use(VueTypedJs)
 
 export default {
   name: 'Init',
+  created() {
+    this.$options.sockets.onmessage = (data) => {
+      let JSONized = JSON.parse(data.data)
+      if (JSONized.operation === "presentator") {
+        console.log("Ricevuto token presentatore")
+        console.log(JSONized.arg)
+        new QRCode(this.$refs.qrcode, {
+          text: JSONized.arg,
+          logoBackgroundTransparent: true,
+          width: screen.width / 100 * 40,
+          height: screen.width / 100 * 40,
+          drawer: "svg"
+        });
+      }
+    }
+  },
   methods: {
     translate: function () {
-      document.getElementById("text").classList.add("translate");
-      document.getElementById("text").addEventListener("animationend", function (){
-        document.getElementById("logo").style.display = "block";
+      let textelement = document.getElementById("text")
+      let logoelement = document.getElementById("logo")
+      let qrcodeelement = document.getElementById("qrcode")
+
+      textelement.classList.add("translate");
+      textelement.addEventListener("animationend", function () {
+        logoelement.style.display = "block";
+      })
+      logoelement.addEventListener("animationend", function () {
+        qrcodeelement.style.display = "block";
       })
     },
     translatetopleft: function (elementid) {
@@ -34,9 +59,9 @@ export default {
 }
 </script>
 <style scoped>
-#logo{
+#logo {
   position: absolute;
-  left:50%;
+  left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
   display: none;
@@ -45,37 +70,77 @@ export default {
   animation-fill-mode: forwards;
   animation-duration: 2s;
 }
+
 @keyframes translate {
-  100% {opacity: 0%}
+  100% {
+    opacity: 0%
+  }
 }
+
 @keyframes logoanim {
-  0% {width: 0%}
-  65% {width: 20%}
-  80% {width: 10%}
-  100% {width: 40%}
+  0% {
+    width: 0%
+  }
+  32.5% {
+    width: 20%
+  }
+  40% {
+    width: 10%
+  }
+  50% {
+    width: 40%
+  }
+  100% {
+    width: 20%
+  }
 }
+
 @keyframes translatetopleft {
-  100% {top: 20px; left: 20px;}
+  100% {
+    top: 20px;
+    left: 20px;
+  }
 }
+
+@keyframes fadein {
+  100% {
+    opacity: 100%;
+  }
+}
+
 #all {
   background-color: black;
   position: absolute;
   width: 100%;
   height: 100%;
 }
-h1{
+
+#qrcode, svg {
+  display: none;
+  opacity: 0%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  animation: fadein;
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+}
+
+h1 {
   text-align: center;
   color: white;
 }
-.translate{
+
+.translate {
   animation-duration: 3s;
   animation-name: translate;
   animation-fill-mode: forwards;
 }
 
-#text{
+#text {
   position: absolute;
-  left:50%;
+  left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
 }
